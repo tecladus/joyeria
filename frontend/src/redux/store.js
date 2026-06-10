4ada4dfdf7fa46bb6dbae0aaf468e15cbfd7d51d
Ivan <karkoszkaivan@gmail.com>
2026-06-10 18:47:11 +0000

    Fix: storage de redux-persist roto en produccion (interop CJS) - importar desde redux-persist/es/storage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -9,7 +9,8 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// Importar desde es/ (ESM): el build de lib/ (CommonJS) rompe el interop en el bundle de producción
+import storage from 'redux-persist/es/storage';
 import { configurarObtencionToken } from '../services/api';
 import authReducer from './slices/authSlice';
 import carritoReducer from './slices/carritoSlice';
